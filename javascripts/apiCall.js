e0fcf664dc07a698f9c65043b2e22f19d6da9a84
Taylor Harry <taylor.andrew.harry@gmail.com>
2016-11-08 19:01:20 -0600

    added updates to project

--- javascripts/apiCall.js
+++ javascripts/apiCall.js
@@ -8,7 +8,6 @@ var movieHistor = (function() {
                     url: `http://www.omdbapi.com/?t="${userMovie}"&y=&plot=short&r=json`
                 }).then((response) => {
                     resolve(response);
-                    console.log("response from api title call", response);
                 }, (error) => {
                     reject(error);
                 });
